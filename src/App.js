3ece3e01378fbe618bf3666699810cba2a94a3d6
Flavia Lisboa <lisboafla16@gmail.com>
2022-01-31 21:06:04 +0100

    start build forecast

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 import './App.css';
+import Weather from "./Weather";
 
 function App() {
   return (
     <div className="App"> 
-    <div className = " container">
+    <div className = "container">
     
-    Weather App
+     < Weather />
         <footer>
           This project was coded by Flavia Lisboa and is {" "}
       
